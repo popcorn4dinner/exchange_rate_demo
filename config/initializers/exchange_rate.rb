8e57815bd807e6d0cf849c9bfb032f1f287c238a
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-30 14:04:45 +0200

    exchange_rate gem configuration

--- config/initializers/exchange_rate.rb
+++ config/initializers/exchange_rate.rb
@@ -1,4 +1,7 @@
+require 'exchange_rate'
+require 'exchange_rate/providers/ecb'
+
 ExchangeRate.configure do |c|
   c.provider = ExchangeRate::Providers::Ecb
-  c.data_source_path = Rails.root.join('tmp', 'change_rates.xml')
+  c.data_source_path = Rails.root.join('tmp', 'exchange_rates.xml')
 end
